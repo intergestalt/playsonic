a8948e5579fd2c11b52b344494b95d85204ee35d
Holger Heißmeyer <holgheiss@gmail.com>
2017-11-09 18:01:45 +0100

    add link field to member

--- site/snippets/member.php
+++ site/snippets/member.php
@@ -2,6 +2,10 @@
 
 <?php echo $member->text()->kirbytext() ?>
 
+<?php if ($member->link()) : ?>
+  <a target="_blank" href="<?= $member->link();?>"><?= $member->link() ?></a>
+<?php endif; ?>
+
 <?php if($image = $member->images()->sortBy('sort', 'asc')->first()): $thumb = $image->crop(600, 600); ?>
 	<img src="<?= $thumb->url() ?>" alt="Thumbnail for <?= $member->title()->html() ?>" class="" />
 <?php endif ?>
