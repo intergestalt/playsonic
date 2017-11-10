0ba8e58a570ad37cb78d4d1e60a7e74327a81ef7
Holger Heißmeyer <holgheiss@gmail.com>
2017-11-10 15:40:10 +0100

    debug.scss

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,44 @@ $(window).load(function(){
 		fragment: '#pjax-container'
 	})	
 })
+
+/* ------ move speed effect ------------- */
+
+$.fn.moveIt = function(){
+  var $window = $(window);
+  var instances = [];
+  
+  $(this).each(function(){
+    instances.push(new moveItItem($(this)));
+  });
+  
+  $(window).on('scroll resize', function(){
+    var scrollTop = $window.scrollTop();
+    instances.forEach(function(inst){
+      inst.update(scrollTop);
+    });
+  })
+}
+
+var moveItItem = function(el){
+  this.el = $(el);
+  this.speed = parseInt(this.el.attr('data-scroll-speed'));
+};
+
+moveItItem.prototype.update = function(scrollTop){
+  var translateY = 0;
+  var marginBottom = 0;
+
+  if (window.innerWidth >= breakpoint) {
+    translateY = -(scrollTop / this.speed);
+    marginBottom = -(scrollTop / this.speed);
+  }
+
+  this.el.css('transform', 'translateY(' + translateY + 'px)');
+  this.el.css('margin-bottom', marginBottom + 'px');
+};
+
+// Initialization
+$(function(){
+  $('[data-scroll-speed]').moveIt();
+});
