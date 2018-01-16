e93582267c6901fd9c4365dec9a5f9d9b190d32e
Sebastian Quack <sebastianquack@gmail.com>
2018-01-16 17:59:16 +0100

    logo link fix

---
+++ site/snippets/partner-logos.php
@@ -0,0 +1,9 @@
+<div class="partner-logo-container">  	
+  	<img src="<?= $kirby->urls()->assets() ?>/images/partner-logos.png" alt="partner logos" usemap="partner-logos"/>	
+  	
+  	<a target="_blank" href="https://www.alteoper.de/" title="Alte Oper Frankfurt"></a>
+  	<a target="_blank" style="left: 50%" href="http://ensemble-modern.com/" title="Ensemble Modern"></a>
+  	<a target="_blank" style="top: 50%" href="http://hfmdk-frankfurt.info" title="HfMDK Frankfurt am Main"></a>
+	<a target="_blank" style="top: 50%; left: 50%;" href="https://www.deutsche-bank-stiftung.de/" title="Deutsche Bank Stiftung"></a>
+	
+</div>
--- site/templates/home.php
+++ site/templates/home.php
@@ -2,18 +2,8 @@
   <h1><?= $page->headline()->isEmpty() ? $page->title()->html() : $page->headline() ?></h1>
   <div><?= $page->text()->kirbytext() ?></div>
 
-  <div class="partner-logo-container">  	
-  	<img src="<?= $kirby->urls()->assets() ?>/images/partner-logos.png" alt="partner logos" usemap="partner-logos"/>	
-  	
-  	<map name="partner-logos">
-  		<area shape="rect" coords="0,0,445,220" href="https://www.alteoper.de/" alt="Alte Oper Frankfurt">
-  		<area shape="rect" coords="500,0,1200,220" href="http://ensemble-modern.com/" alt="Ensemble Modern">
-  		<area shape="rect" coords="0,245,445,697" href="http://hfmdk-frankfurt.info" alt="HfMDK Frankfurt am Main">
-  		<area shape="rect" coords="500,245,1200,697" href="https://www.deutsche-bank-stiftung.de/" alt="Deutsche Bank Stiftung">
-	</map>
-
-  </div>
-
+  <?php snippet('partner-logos') ?>
+  
   <div class="legal">
   	<a href="/<?= $site->language()?>/impressum"><?= $site->page('impressum') ? $site->page('impressum')->title() : "" ?></a>
   	<a href="/<?= $site->language()?>/datenschutz"><?= $site->page('datenschutz') ? $site->page('datenschutz')->title() : "" ?></a>
