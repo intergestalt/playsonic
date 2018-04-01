e4f447457e05031c99c8ca4a12727a2b92e2b155
Sebastian Quack <sebastianquack@gmail.com>
2018-04-01 13:46:45 +0200

    language correction

--- site/languages/de.php
+++ site/languages/de.php
@@ -3,7 +3,7 @@
 l::set('blog_read_more', 'weiterlesen');
 l::set('blog_empty', 'Hier sind noch keine Blog-Einträge vorhanden.');
 l::set('menu', 'menü');
-l::set('back_to_all_teams', 'alle FOKUS TEAMS in der Übersicht');
+l::set('back_to_all_teams', 'alle Fokus-Teams in der Übersicht');
 l::set('next_entry', 'nächster Blog-Eintrag');
 l::set('prev_entry', 'vorheriger Blog-Eintrag');
 l::set('more_info', 'Fokus-Team');
--- site/languages/en.php
+++ site/languages/en.php
@@ -3,7 +3,7 @@
 l::set('blog_read_more', 'read more');
 l::set('blog_empty', 'There are no blog entries yet.');
 l::set('menu', 'menu');
-l::set('back_to_all_teams', 'back to overview of all FOCUS TEAMS');
+l::set('back_to_all_teams', 'back to overview of all focus teams');
 l::set('next_entry', 'next blog entry');
 l::set('prev_entry', 'previous blog entry');
 l::set('more_info', 'focus team');
