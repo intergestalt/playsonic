00a717980cd06a50f214df1722dd71a1a2cdd2d7
Sebastian Quack <sebastianquack@gmail.com>
2018-05-21 21:31:12 +0200

    add custom field program

--- site/templates/program.php
+++ site/templates/program.php
@@ -1,6 +1,7 @@
 <?php snippet('header') ?>
 
   <h1><?php echo $page->title()->html() ?></h1>
+  <div><?php echo $page->text()->html() ?></div>
   
   <?php snippet('event-list', ['events' => $page->children()->visible()]) ?>
 
